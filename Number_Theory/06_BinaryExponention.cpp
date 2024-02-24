9dd87450e5cc0bf74859c725d9e8d412f13edc90
SastaStudent <mc1377929@gmail.com>
2024-02-24 23:52:12 +0530

    For larger n <=10^18

--- Number_Theory/06_BinaryExponention.cpp
+++ Number_Theory/06_BinaryExponention.cpp
@@ -5,7 +5,7 @@ using namespace std::chrono;
 
 const int mod = 1e9 + 7;
 
-long binaryExpo(int n, int k) {
+long binaryExpo1(int n, int k) {
     long res = 1;
     while (k > 0) {
         if (k & 1) {
@@ -19,12 +19,28 @@ long binaryExpo(int n, int k) {
     }
     return res;
 }
+//for larger n<=10^18
+long long  binaryExpo(long long  n, long long k) {
+    long long  res = 1;
+    n=n%mod;
+    while (k > 0) {
+        if (k & 1) {
+            // odd (last set bit)
+            res = ((res%mod) *(n%mod)) % mod;
+        }
+        // even divide by two
+        // right shift
+        n = ((n%mod)* (n%mod)) % mod;
+        k = k >> 1;
+    }
+    return res;
+}
 
 int main() {
     int t;
     std::cin >> t;
     while (t--) {
-        int n, k;
+        long long n, k;
         std::cin >> n >> k;
         auto start = high_resolution_clock::now();
         std::cout << binaryExpo(n, k) << std::endl;
