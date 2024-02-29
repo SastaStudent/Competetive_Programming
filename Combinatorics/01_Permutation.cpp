1e3c08cf985925b556d016d3ef5f81e5b072cba7
SastaStudent <mc1377929@gmail.com>
2024-03-01 02:46:27 +0530

    Combinatorics Folder Added

---
+++ Combinatorics/01_Permutation.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int tc;
+    cin>>tc;
+    while(tc--)
+    {
+        
+    }
+    return 0;
+}
